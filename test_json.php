7456ab175fbcf17218c85641c2e9ea541d7e40ba
shaquib <shaquib@users.noreply.github.com>
2016-03-12 12:24:55 +0530

    Create test_json.php

---
+++ test_json.php
@@ -0,0 +1,61 @@
+<?php
+
+if (!function_exists('json_encode'))
+{
+  function json_encode($a=false)
+  {
+    if (is_null($a)) return 'null';
+    if ($a === false) return 'false';
+    if ($a === true) return 'true';
+    if (is_scalar($a))
+    {
+      if (is_float($a))
+      {
+        // Always use "." for floats.
+        return floatval(str_replace(",", ".", strval($a)));
+      }
+
+      if (is_string($a))
+      {
+        static $jsonReplaces = array(array("\\", "/", "\n", "\t", "\r", "\b", "\f", '"'), array('\\\\', '\\/', '\\n', '\\t', '\\r', '\\b', '\\f', '\"'));
+        return '"' . str_replace($jsonReplaces[0], $jsonReplaces[1], $a) . '"';
+      }
+      else
+        return $a;
+    }
+    $isList = true;
+    for ($i = 0, reset($a); $i < count($a); $i++, next($a))
+    {
+      if (key($a) !== $i)
+      {
+        $isList = false;
+        break;
+      }
+    }
+    $result = array();
+    if ($isList)
+    {
+      foreach ($a as $v) $result[] = json_encode($v);
+      return '[' . join(',', $result) . ']';
+    }
+    else
+    {
+      foreach ($a as $k => $v) $result[] = json_encode($k).':'.json_encode($v);
+      return '{' . join(',', $result) . '}';
+    }
+  }
+}
+
+
+$conn = mysqli_connect('localhost','root','') or die("could not connect to database");
+
+$select_db=mysqli_select_db($conn, 'feedback') or die("could not connect");
+session_start();
+$query = mysqli_query($conn, "SELECT * FROM feed");
+$rows = array();
+while($row = mysqli_fetch_assoc($query)) {
+    $rows[] = $row;
+}
+echo json_encode($rows);
+
+?>
